21a234edd5eea100043ee6287184cb3af824c4cd
Oleksii Suriadnyi <oleksii.suriadnyi@ericsson.com>
2018-04-13 16:18:34 +0300

    Add project files.

---
+++ AzureBlobStorageEmulatorAuthentication/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Xml.Linq;
+
+namespace AzureBlobStorageEmulatorAuthentication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Construct the URI. This will look like this:
+            //   https://myaccount.blob.core.windows.net/resource
+            //String uri = string.Format("http://{0}.blob.core.windows.net?comp=list", storageAccountName);
+
+            String storageAccountName = "devstoreaccount1";
+            String storageAccountKey = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==";
+            String uri = string.Format("http://127.0.0.1:10000/{0}/?comp=list", storageAccountName);
+
+            // Set this to whatever payload you desire. Ours is null because we're not passing anything in.
+            Byte[] requestPayload = null;
+
+            //Instantiate the request message with a null payload.
+            using (var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri)
+                { Content = (requestPayload == null) ? null : new ByteArrayContent(requestPayload) })
+            {
+                // Add the request headers for x-ms-date and x-ms-version.
+                DateTime now = DateTime.UtcNow;
+                httpRequestMessage.Headers.Add("x-ms-date", now.ToString("R", CultureInfo.InvariantCulture));
+                httpRequestMessage.Headers.Add("x-ms-version", "2017-04-17");
+                // If you need any additional headers, add them here before creating the authorization header. 
+
+                // Get the authorization header and add it.
+                httpRequestMessage.Headers.Authorization = GetAuthorizationHeader(storageAccountName, storageAccountKey, now, httpRequestMessage);
+
+                var cancellationToken = default(System.Threading.CancellationToken);
+                // Send the request.
+                using (HttpResponseMessage httpResponseMessage = new HttpClient().SendAsync(httpRequestMessage, cancellationToken).GetAwaiter().GetResult())
+                {
+                    // If successful (status code = 200), parse the XML response for the container names.
+                    if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+                    {
+                        String xmlString = httpResponseMessage.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                        XElement x = XElement.Parse(xmlString);
+                        foreach (XElement container in x.Element("Containers").Elements("Container"))
+                        {
+                            Console.WriteLine("Container name = {0}", container.Element("Name").Value);
+                        }
+                    }
+                }
+            }
+        }
+
+        internal static AuthenticationHeaderValue GetAuthorizationHeader(
+            string storageAccountName, string storageAccountKey, DateTime now,
+            HttpRequestMessage httpRequestMessage, string ifMatch = "", string md5 = "")
+        {
+            // This is the raw representation of the message signature.
+            HttpMethod method = httpRequestMessage.Method;
+            String MessageSignature = String.Format("{0}\n\n\n{1}\n{5}\n\n\n\n{2}\n\n\n\n{3}{4}",
+                        method.ToString(),
+                        (method == HttpMethod.Get || method == HttpMethod.Head) ? String.Empty
+                          : httpRequestMessage.Content.Headers.ContentLength.ToString(),
+                        ifMatch,
+                        GetCanonicalizedHeaders(httpRequestMessage),
+                        GetCanonicalizedResource(httpRequestMessage.RequestUri, storageAccountName),
+                        md5);
+
+            // Now turn it into a byte array.
+            byte[] SignatureBytes = Encoding.UTF8.GetBytes(MessageSignature);
+
+            // Create the HMACSHA256 version of the storage key.
+            HMACSHA256 SHA256 = new HMACSHA256(Convert.FromBase64String(storageAccountKey));
+
+            // Compute the hash of the SignatureBytes and convert it to a base64 string.
+            string signature = Convert.ToBase64String(SHA256.ComputeHash(SignatureBytes));
+
+            // This is the actual header that will be added to the list of request headers.
+            AuthenticationHeaderValue authHV = new AuthenticationHeaderValue("SharedKey",
+                storageAccountName + ":" + Convert.ToBase64String(SHA256.ComputeHash(SignatureBytes)));
+            return authHV;
+        }
+
+        private static string GetCanonicalizedHeaders(HttpRequestMessage httpRequestMessage)
+        {
+            var headers = from kvp in httpRequestMessage.Headers
+                          where kvp.Key.StartsWith("x-ms-", StringComparison.OrdinalIgnoreCase)
+                          orderby kvp.Key
+                          select new { Key = kvp.Key.ToLowerInvariant(), kvp.Value };
+
+            StringBuilder sb = new StringBuilder();
+
+            // Create the string in the right format; this is what makes the headers "canonicalized" --
+            // it means put in a standard format. http://en.wikipedia.org/wiki/Canonicalization
+            foreach (var kvp in headers)
+            {
+                StringBuilder headerBuilder = new StringBuilder(kvp.Key);
+                char separator = ':';
+
+                // Get the value for each header, strip out \r\n if found, then append it with the key.
+                foreach (string headerValues in kvp.Value)
+                {
+                    string trimmedValue = headerValues.TrimStart().Replace("\r\n", String.Empty);
+                    headerBuilder.Append(separator).Append(trimmedValue);
+
+                    // Set this to a comma; this will only be used 
+                    //   if there are multiple values for one of the headers.
+                    separator = ',';
+                }
+                sb.Append(headerBuilder.ToString()).Append("\n");
+            }
+            return sb.ToString();
+        }
+
+        private static string GetCanonicalizedResource(Uri address, string storageAccountName)
+        {
+            // The absolute path will be "/" because for we're getting a list of containers.
+            StringBuilder sb = new StringBuilder("/").Append(storageAccountName).Append(address.AbsolutePath);
+
+            // Address.Query is the resource, such as "?comp=list".
+            // This ends up with a NameValueCollection with 1 entry having key=comp, value=list.
+            // It will have more entries if you have more query parameters.
+            NameValueCollection values = HttpUtility.ParseQueryString(address.Query);
+
+            foreach (var item in values.AllKeys.OrderBy(k => k))
+            {
+                sb.Append('\n').Append(item).Append(':').Append(values[item]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
